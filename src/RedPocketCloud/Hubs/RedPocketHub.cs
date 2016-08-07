19cd5774dfce84b101bc22bdb3a4176f824c7d40
あまみや ゆうこ <1@1234.sh>
2016-08-07 17:29:52 +0800

    Implement hub

---
+++ src/RedPocketCloud/Hubs/RedPocketHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace RedPocketCloud.Hubs
+{
+    public class RedPocketHub : Hub
+    {
+        public void Join(string name)
+        {
+            Groups.Add(Context.ConnectionId, name);
+        }
+    }
+}
