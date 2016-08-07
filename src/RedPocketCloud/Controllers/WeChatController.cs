c501ca515daf54fe767a63fd48ef70e706293fea
あまみや ゆうこ <1@1234.sh>
2016-08-07 17:58:50 +0800

    Implement wechat controller

--- src/RedPocketCloud/Controllers/WeChatController.cs
+++ src/RedPocketCloud/Controllers/WeChatController.cs
@@ -1,16 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using static RedPocketCloud.Common.Wxpay;
 
 namespace RedPocketCloud.Controllers
 {
+    public enum Operation
+    {
+        CallBack,
+        RedPocket,
+        Wallet
+    }
+
     public class WeChatController : BaseController
     {
-        public IActionResult RedPocket(string Username)
+        private string OperationToRoute(string Merchant, Operation Operation)
         {
-            return View(GetActivityByUserId(Username));
+            var prefix = HttpContext.Request.Scheme + "://" + HttpContext.Request.Host + "/WeChat/";
+            var postfix = "/" + Merchant;
+            switch (Operation)
+            {
+                case Operation.CallBack:
+                    return prefix + "CallBack" + postfix;
+                case Operation.RedPocket:
+                    return prefix + "RedPocket" + postfix;
+                case Operation.Wallet:
+                    return prefix + "Wallet" + postfix;
+                default:
+                    return null;
+            }
+        }
+
+        [HttpGet]
+        [Route("[controller]/Entry/{Merchant}/{Operation}")]
+        public IActionResult Entry(string Merchant, Operation Operation)
+        {
+            if (string.IsNullOrWhiteSpace(HttpContext.Session.GetString("OpenId")) || Convert.ToDateTime(HttpContext.Session.GetString("Expire")) <= DateTime.Now)
+                return Redirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + Startup.Config["WeChat:AppId"] + "&redirect_uri=" + UrlEncoder.Default.Encode(OperationToRoute(Merchant, Operation.CallBack) + "?next=" + OperationToRoute(Merchant, Operation)) + "&response_type=code&scope=snsapi_userinfo");
+            else
+                return Redirect(OperationToRoute(Merchant, Operation));
+        }
+
+        [HttpGet]
+        [Route("[controller]/CallBack/{Merchant}")]
+        public async Task<IActionResult> CallBack(string Merchant, string code, string next)
+        {
+            try
+            {
+                var oid = await AuthorizeAsync(code);
+                HttpContext.Session.SetString("OpenId", oid.Id);
+                HttpContext.Session.SetString("AccessToken", oid.AccessToken);
+                HttpContext.Session.SetString("Expire", oid.AccessTokenExpire.ToString());
+                HttpContext.Session.SetString("Nickname", oid.NickName);
+                HttpContext.Session.SetString("AvatarUrl", oid.AvatarUrl);
+                return Redirect(next);
+            }
+            catch
+            {
+                return RedirectToAction("Entry", "WeChat", new { id = Merchant, Operation = Operation.RedPocket });
+            }
+        }
+
+        [HttpGet]
+        public IActionResult RedPocket(string Merchant)
+        {
+            if (string.IsNullOrWhiteSpace(HttpContext.Session.GetString("OpenId")) || Convert.ToDateTime(HttpContext.Session.GetString("Expire")) <= DateTime.Now)
+                return RedirectToAction("Entry", "WeChat", new { Merchant = Merchant, Operation = Operation.RedPocket });
+            return View(GetActivityByUserId(Merchant));
         }
     }
 }
