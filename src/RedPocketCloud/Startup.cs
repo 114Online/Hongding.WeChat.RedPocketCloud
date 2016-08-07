ff166722a14740f9956ece639d9080105134ee83
あまみや ゆうこ <1@1234.sh>
2016-08-07 17:25:26 +0800

    Fixed index host display

--- src/RedPocketCloud/Startup.cs
+++ src/RedPocketCloud/Startup.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
