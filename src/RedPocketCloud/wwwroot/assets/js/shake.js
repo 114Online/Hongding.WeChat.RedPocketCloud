983df3b7ce0fffc53a2594863c725d3811762b4a
あまみや ゆうこ <1@1234.sh>
2016-08-09 18:41:47 +0800

    Implement responsive drawn display

--- src/RedPocketCloud/wwwroot/assets/js/shake.js
+++ src/RedPocketCloud/wwwroot/assets/js/shake.js
@@ -27,6 +27,15 @@ $(document).ready(function () {
             lastShakeZ = curShakeZ;
         }
     }
+
+    $('.pending').click(function () { Close(); });
+    $('.drawn').click(function () { Close(); });
+    $('.undrawn').click(function () { Close(); });
+
+    var font_size = 0.048 * $(window).height();
+    $('.drawn-text').css('font-size', font_size + "px");
+    $('.drawn-text').css('line-height', font_size + "px");
+    $('.drawn-text').css('margin-top', -(font_size / 2.0) + "px");
 });
 
 function Shake()
@@ -57,10 +66,23 @@ function Close()
     $('.drawn').removeClass('showing');
     $('.undrawn').removeClass('showing');
     $('.alpha').removeClass('active');
+    $('.drawn-text').removeClass('active');
 }
 
 function ShowPending()
 {
     $('.alpha').addClass('active');
     $('.pending').addClass('showing');
+}
+
+function ShowUndrawn() {
+    $('.alpha').addClass('active');
+    $('.undrawn').addClass('showing');
+}
+
+function ShowDrawn(txt, url) {
+    $('.alpha').addClass('active');
+    $('.drawn-text').addClass('active');
+    $('.drawn').addClass('showing');
+    $('.drawn-text').text(txt);
 }
